3db2521caaa93ee547b60d696a57e7250f50eca5
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-09 14:02:53 +0100

    checkout

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -22,7 +22,7 @@ const page = () => {
   }
 
   return (
-    <div className="mt-10 px-10 mb-64">
+    <div className="mt-10 px-6 lg:px-10 mb-64">
       <div className="flex items-center gap-1 poppins">
         <Link className="text-[#646261]" href="/">Home</Link>
         <span className="text-[#646261]"><FaAngleRight /></span>
@@ -32,16 +32,16 @@ const page = () => {
       </div>
 
       <div className="">
-        <h1 className="text-center bgpoppins font-extrabold text-[2.6rem]">Checkout</h1>
-        <div className="flex justify-around mt-10">
-          <div className="mt-10">
+        <h1 className="text-center poppins font-extrabold mt-10 lg:mt-0  text-2xl lg:text-[2.6rem]">Checkout</h1>
+        <div className="flex flex-col lg:flex-row justify-around mt-5 lg:mt-10">
+          <div className="mt-10 hidden lg:block">
             <SelectedItems width={width} padding={padding} items={cartItems} removeFromCart={removeFromCart} updateQuantity={updateQuantity} />
           </div>
 
           {cartItems.length > 0 ? (
             <>
               <div>
-                <div className="mb-5 flex gap-2 items-center text-[#888787] poppins font-semibold text-2xl">
+                <div className="hidden mb-5 lg:flex gap-2 items-center text-[#888787] poppins font-semibold text-2xl">
                   <span className="text-[#3A4980]">Details</span>
                   <span className="text-[#c7c5c5]"><GoDash /></span>
                   <span className="text-[#c7c5c5]"><FaCircleCheck /></span>
@@ -54,11 +54,11 @@ const page = () => {
                 </div>
 
                 {payment === false ? (<>
-                  <form className="text-[#585858] poppins font-medium max-w-[550px]">
-                    <div className="bg-plainWhite p-10">
-                      <h3 className="text-xl">Contact Details</h3>
+                  <form className="text-[#585858] poppins lg:font-medium max-w-[550px]">
+                    <div className="lg:bg-plainWhite p-4 lg:p-10">
+                      <h3 className="lg:text-xl">Contact Details</h3>
 
-                      <div className="flex gap-5 my-5">
+                      <div className="text-xs flex flex-col lg:flex-row gap-5 my-5">
                         <div>
                           <label htmlFor="firstName">First Name</label>
                           <input type="text" id="firstName" className='input' />
@@ -70,33 +70,33 @@ const page = () => {
                         </div>
                       </div>
 
-                      <div className="my-5">
+                      <div className="my-5 text-xs">
                         <label htmlFor="email">Email</label>
                         <input type="emai" id="email" className="input" />
                       </div>
 
 
-                      <div className="my-5">
+                      <div className="my-5 text-xs">
                         <label htmlFor="phone">Phone Number</label>
                         <input type="text" id="phone" className='input' />
                       </div>
                     </div>
 
-                    <div className="bg-plainWhite p-10 mt-10">
-                      <h3 className="text-xl">Shipping Details</h3>
+                    <div className="lg:bg-plainWhite p-4 lg:p-10 mt-10">
+                      <h3 className="lg:text-xl">Shipping Details</h3>
 
-                      <div className="my-5">
+                      <div className="my-5 text-xs">
                         <label htmlFor="flat">Flat/House no.</label>
                         <input type="text" id="flat" className='input' />
                       </div>
 
-                      <div className="my-5">
+                      <div className="my-5 text-xs">
                         <label htmlFor="address">Address</label>
                         <input type="text" id="address" className="input" />
                       </div>
 
 
-                      <div className="flex gap-5 my-5">
+                      <div className="flex flex-col lg:flex-row gap-5 text-xs my-5">
                         <div>
                           <label htmlFor="city">City</label>
                           <input type="text" id="city" className='input' />
@@ -108,13 +108,13 @@ const page = () => {
                         </div>
                       </div>
 
-                      <div className="w-1/2 mx-auto">
+                      <div className="lg:w-1/2 lg:mx-auto text-xs">
                         <label htmlFor="postalCode">Postal Code</label>
                         <input type="text" id="postalCode" className='input' />
                       </div>
                     </div>
-                    <div className="flex justify-end mt-8">
-                      <button type="button" className="bg-[#3A4980] text-plainWhite font-extrabold py-3 px-6 rounded-md" onClick={(e) => handleClickContinue(e)}>Continue</button>
+                    <div className="lg:flex p-4 lg:p-0 justify-end mt-8 mx-auto">
+                      <button type="button" className="bg-[#3A4980] text-plainWhite font-extrabold w-full py-2 lg:py-3 lg:px-6 rounded-md" onClick={(e) => handleClickContinue(e)}>Continue</button>
                     </div>
                   </form>
 
