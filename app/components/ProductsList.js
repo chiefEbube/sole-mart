28e85166f327ca2340b2a50359ea86f717245ca1
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-09 09:27:49 +0100

    view product page

--- app/components/ProductsList.js
+++ app/components/ProductsList.js
@@ -27,12 +27,12 @@ const ProductsList = ({ products }) => {
 
   return (
     <div>
-      <div className='inter flex flex-wrap items-center justify-center gap-5'>
+      <div className='inter flex flex-wrap items-center justify-center lg:gap-5'>
         {
           paginatedProducts.map((product) => (
-            <Link href={`/products/${product.id}`}>
+            <Link href={`/products/${product.id}`} className='w-[210px] p-2'>
 
-              <div className='bg-whiteBg mb-6 pb-5 w-[250px] h-auto transition ease-in-out delay-200 hover:scale-110 duration-300 cursor-pointer'>
+              <div className='bg-whiteBg mb-6 pb-5 h-auto transition ease-in-out delay-200 hover:scale-110 duration-300 cursor-pointer'>
                 <div className='w-[95%] h-[249px] relative flex justify-center items-center mx-auto'>
                   <div className='bg-plainWhite absolute right-2 top-3 w-10 h-10 flex items-center justify-center rounded-full'>
                     <FaRegHeart className='text-[#667085] text-xl' />                                                           
@@ -49,20 +49,20 @@ const ProductsList = ({ products }) => {
 
                 <div className='flex flex-col items-center gap-2 p-4'>
                   <div className='flex items-center w-full justify-between'>
-                    <p className='text-[#667085] text-base font-semibold'>{product.name}</p>
-                    <p className='text-md flex items-center font-semibold'>
+                    <p className='text-[#667085] text-sm lg:text-base font-semibold'>{product.name}</p>
+                    <p className='text-md text-sm lg:text-base flex items-center font-semibold'>
                       <TbCurrencyNaira className='text-2xl' />{product.price}
                     </p>
                   </div>
 
-                  <p className='w-full text-[#98A2B3] text-sm'>{product.remaining} types of shoos available</p>
+                  <p className='w-full text-[#98A2B3] text-xs lg:text-sm'>{product.remaining} types of shoos available</p>
                   <div className='flex items-center w-full justify-start'>
                     <GoldStars numberOfStars={product.stars} className={'flex items-center'}/>
-                    <p className='text-sm'>({product.reviews})</p>
+                    <p className='text-xs lg:text-sm'>({product.reviews})</p>
                   </div>
 
                   <Link className='w-full mt-3' href={`/products/${product.id}`}>
-                    <button className="w-full h-8 bg-secondaryBg text-plainWhite text-sm rounded-3xl inter font-semibold hover:opacity-90 duration-300">View Details</button>
+                    <button className="w-full h-7 lg:h-8 bg-secondaryBg text-plainWhite text-xs lg:text-sm rounded-3xl inter font-semibold hover:opacity-90 duration-300">View Details</button>
                   </Link>
                 </div>
 
--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -22,28 +22,28 @@ export default function ProductDetailPage({ params }) {
   const relatedProducts = productsData.filter((p) => p.id !== product.id);
 
   return (
-    <div className="container mx-auto mb-16">
+    <div className="mb-16">
       <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto my-10">
-        <div className="w-[450px] h-[450px]">
+        <div className="w-[400px] h-[400px] lg:w-[450px] lg:h-[450px] mx-auto">
           <img src={product.image} alt={product.name} className="object-cover h-full w-full" />
         </div>
 
-        <div className="flex flex-col justify-evenly">
-          <h1 className="mb-2 font-bold text-5xl uppercase text-[#231F20] poppins">{product.name}</h1>
+        <div className="flex flex-col justify-evenly mt-4">
+          <h1 className="mb-2 font-bold text-3xl lg:text-5xl uppercase text-[#231F20] poppins">{product.name}</h1>
           <GoldStars numberOfStars={product.stars} className={'flex'} />
           <p className="poppins mt-4 mr-8">{product.stars}/5</p>
           <p className="text-xl font-bold text-[#eb2828d4] text-gray-800 flex items-center"><TbCurrencyNaira className="inline text-2xl" />{product.price}</p>
-          <p className="poppins text-[#0000007a] my-4">{product.description}</p>
+          <p className="poppins text-[#0000007a] my-4 text-sm lg:text-base">{product.description}</p>
           <section className="flex poppins text-base text-[#526475] gap-5 font-medium">
-            <div className="flex items-center bg-[#f1f1f0] py-3 px-4  rounded-3xl">
+            <div className="flex items-center bg-[#f1f1f0] py-3 px-4 text-sm lg:text-base rounded-3xl">
               <span>Class</span>
             </div>
-            <div className="flex items-center bg-[#f1f1f0] py-2 px-4 rounded-3xl">
+            <div className="flex items-center bg-[#f1f1f0] py-2 px-4 text-sm lg:text-base rounded-3xl">
               <span>Comfort</span>
             </div>
           </section>
           <div>
-            <button onClick={() => addToCart(product.id)} className="w-10/12 bg-secondaryBg text-plainWhite text-sm rounded-3xl inter font-semibold hover:opacity-90 duration-300 mt-12 py-4">Add to Cart
+            <button onClick={() => addToCart(product.id)} className="w-full lg:w-10/12 bg-secondaryBg text-plainWhite text-sm rounded-3xl inter font-semibold hover:opacity-90 duration-300 mt-12 py-4">Add to Cart
             </button>
           </div>
         </div>
