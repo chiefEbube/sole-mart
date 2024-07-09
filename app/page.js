631fff9f7a964662301098bb397d0d626ddcdd29
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-09 09:02:11 +0100

    finished page

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="mb-24 w-screen">
+    <div className="mb-24">
       <header className="bg-greyBg flex items-center lg:justify-between mx-4 lg:mx-16 p-4 lg:pl-14 rounded-2xl"> 
-        <div className="">
+        <div className="mr-18">
           <h3 className="text-primaryBg poppins text-xl w-[170px] lg:text-[2.5rem] lg:w-[457px] lg:h-[162px] font-extrabold lg:font-bold">Grab Up to 5% Off On Selected Shoe</h3>
           <Link href={"#collections"}>
             <div className="lg:ml-12 flex items-center justify-center w-28 py-2 lg:py-0 mt-4 lg:mt-0 lg:w-[136px] lg:h-[49px] bg-btn text-plainWhite text-sm lg:text-base rounded-xl lg:rounded-3xl inter font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:opacity-90 duration-300">
@@ -18,7 +18,7 @@ export default function Home() {
           </Link>
 
         </div>
-          <div className="lg:w-[612px] lg:h-[612px] -mt-12 -mr-12 lg:-mr-12"> 
+          <div className="h-full lg:w-[612px] lg:h-[612px] -mr-6 -mt-12 lg:-mr-12"> 
           <img src="/assets/images/black-shoe.png" className="w-full h-full object-cover"/>
           </div>
       </header >
@@ -35,7 +35,7 @@ export default function Home() {
         <ProductsList products={productsData} />
       </section>
 
-      {/* <section className="w-full bg-[#D9D9D9] p-3 lg:px-10 lg:py-5 mt-16">
+      <section className="w-full bg-[#D9D9D9] p-3 lg:px-10 lg:py-5 mt-16">
         <div className="flex gap-5 lg:w-7/8 lg:mx-auto items-center justify-around">
           <Image src='/assets/images/profile.png' width={150} height={150} />
           <div className="lg:w-3/5 openSans">
@@ -46,7 +46,7 @@ export default function Home() {
           </div>
         </div>
 
-      </section> */}
+      </section>
     </div >
   );
 }
