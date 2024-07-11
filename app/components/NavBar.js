a2df9c75cfd1b3d19f1e2f6b505fb17e573f2abd
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-11 15:27:12 +0100

    check

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -15,20 +15,28 @@ const NavBar = () => {
 
   return (
     <nav className="lg:container flex items-center justify-between my-4 lg:my-8">
-      <Link className="flex items-center gap-1" href="/">
+      <Link className="flex items-center gap-1 pl-3 md:pl-0" href="/">
         <Image
           src="/assets/images/logo.svg"
           width={80}
           height={78.1}
+          className='hidden md:block'
         />
-        <h3 className="poller text-xl lg:text-3xl text-primaryBg uppercase">Sole Mart</h3>
+
+        <Image
+          src="/assets/images/logo.svg"
+          width={20}
+          height={20}
+          className='block md:hidden'
+        />
+        <h3 className="poller text-[13px] md:text-xl lg:text-3xl text-primaryBg uppercase">Sole Mart</h3>
       </Link>
       <div className="hidden xl:flex items-center gap-14 poppins text-base">
         <Link className={`relative ${pathname === '/' ? 'active' : ''}`} href="/">Product Listing Page<span></span></Link>
         <Link className={`relative ${pathname === '/cart' ? 'active' : ''}`} href="/cart">Cart<span></span></Link>
-        <Link className={`relative`} href="/checkout">Checkout<span></span></Link>
+        <Link className={`relative ${pathname === '/checkout' ? 'active' : ''}`} href="/checkout">Checkout<span></span></Link>
       </div>
-      <div className="flex items-center gap-8 pr-5">
+      <div className="flex items-center gap-3 md:gap-8 pr-5">
         <label class="relative hidden md:block">
           <span class="sr-only">Search</span>
           <input class="placeholder:text-slate-40 poppins block bg-[#f5f5f5] w-60 h-8 rounded-xs pl-8 text-xs" placeholder="What are you looking for" type="text" name="search" />
@@ -37,9 +45,18 @@ const NavBar = () => {
           </span>
         </label>
 
+        <label class="relative md:hidden">
+          <span class="sr-only">Search</span>
+          <input class="placeholder:text-slate-40 poppins block bg-[#f5f5f5] w-40 h-6 rounded-xs pl-2 text-[10px]" placeholder="What are you looking for" type="text" name="search" />
+          <span class="absolute inset-y-0 right-0 flex items-center">
+            <CiSearch className="h-6 w-6" />
+          </span>
+        </label>
+
 
 
         <div>
+
           <Link href="/cart" className={`${pathname === '/cart' ? 'text-[#FF3333]' : ''}`}>
             <span className="relative">
               <GrCart className="h-6 w-6" />
